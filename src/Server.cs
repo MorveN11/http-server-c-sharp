2039fb0a873f6414aaacacdde3f11b7fb69c94fa
MorveN11 <morales.patty.jose@gmail.com>
2024-04-26 20:54:16 -0400

    feat(*): add initial server

--- src/Server.cs
+++ src/Server.cs
@@ -1,10 +1,9 @@
 using System.Net;
 using System.Net.Sockets;
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-Console.WriteLine("Logs from your program will appear here!");
+const int HttpPort = 4221;
 
 // Uncomment this block to pass the first stage
-// TcpListener server = new TcpListener(IPAddress.Any, 4221);
-// server.Start();
-// server.AcceptSocket(); // wait for client
+TcpListener server = new TcpListener(IPAddress.Any, HttpPort);
+server.Start();
+server.AcceptSocket(); // wait for client
