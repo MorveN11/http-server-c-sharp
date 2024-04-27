e08797f03001a7eddff7a12b0086244508d13ca4
MorveN11 <morales.patty.jose@gmail.com>
2024-04-26 22:18:21 -0400

    feat(server): implement first response

--- src/Server.cs
+++ src/Server.cs
@@ -1,9 +1,33 @@
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
 const int HttpPort = 4221;
 
-// Uncomment this block to pass the first stage
 TcpListener server = new TcpListener(IPAddress.Any, HttpPort);
+
 server.Start();
-server.AcceptSocket(); // wait for client
+
+async Task ProcessRequest(Socket socket)
+{
+    var buffer = new byte[1024];
+    var received = await socket.ReceiveAsync(buffer);
+    var request = Encoding.UTF8.GetString(buffer, 0, received);
+    Console.WriteLine(request);
+}
+
+async Task ProcessResponse(Socket socket)
+{
+    var response = "HTTP/1.1 200 OK\r\n\r\n";
+    var buffer = Encoding.UTF8.GetBytes(response);
+    await socket.SendAsync(buffer);
+    socket.Shutdown(SocketShutdown.Both);
+    socket.Dispose();
+}
+
+while (true)
+{
+    var socket = await server.AcceptSocketAsync();
+    await ProcessRequest(socket);
+    await ProcessResponse(socket);
+}
