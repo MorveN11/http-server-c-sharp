35a4ec697590e3c1646fba64d4774f42e8cb8524
MorveN11 <morales.patty.jose@gmail.com>
2024-04-26 22:43:32 -0400

    feat(server): add not found response

--- src/Server.cs
+++ src/Server.cs
@@ -8,26 +8,57 @@ TcpListener server = new TcpListener(IPAddress.Any, HttpPort);
 
 server.Start();
 
-async Task ProcessRequest(Socket socket)
+String getPath(String request)
+{
+    return request.Split(' ')[1];
+}
+
+async Task<bool> IsValidRequest(Socket socket)
 {
     var buffer = new byte[1024];
     var received = await socket.ReceiveAsync(buffer);
     var request = Encoding.UTF8.GetString(buffer, 0, received);
     Console.WriteLine(request);
+    var path = getPath(request);
+    return path == "/";
 }
 
-async Task ProcessResponse(Socket socket)
+void EndConnection(Socket socket)
 {
-    var response = "HTTP/1.1 200 OK\r\n\r\n";
-    var buffer = Encoding.UTF8.GetBytes(response);
-    await socket.SendAsync(buffer);
     socket.Shutdown(SocketShutdown.Both);
     socket.Dispose();
+}
+
+async Task SendResponse(Socket socket, string response)
+{
+    var buffer = Encoding.UTF8.GetBytes(response);
+    await socket.SendAsync(buffer);
+}
+
+async Task ProcessOkResponse(Socket socket)
+{
+    var response = "HTTP/1.1 200 OK\r\n\r\n";
+    await SendResponse(socket, response);
+    EndConnection(socket);
+}
+
+async Task ProcessNotFoundResponse(Socket socket)
+{
+    var response = "HTTP/1.1 404 Not Found\r\n\r\n";
+    await SendResponse(socket, response);
+    EndConnection(socket);
 }
 
 while (true)
 {
     var socket = await server.AcceptSocketAsync();
-    await ProcessRequest(socket);
-    await ProcessResponse(socket);
+    bool isValidRequest = await IsValidRequest(socket);
+    if (isValidRequest)
+    {
+        await ProcessOkResponse(socket);
+    }
+    else
+    {
+        await ProcessNotFoundResponse(socket);
+    }
 }
